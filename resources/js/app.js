ac07511d303a00f0905106eb282615bf9063b702
HyewonDev <hyewon.dev@gmail.com>
2021-04-28 23:39:44 -0400

    add dependencies and set to use font awesome icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(VueRouter);
+
+library.add(faHeart)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 let app = new Vue({
     el: '#app',
--- resources/js/components/Photographer.vue
+++ resources/js/components/Photographer.vue
@@ -36,162 +36,7 @@
             </div>
         </div>
 
-        <div class="gallery-container">
-            <div class="grid gallery-grid cg-24 rg-24">
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape1.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.  thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                                <svg fill="currentColor" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" class="w-85 text-red">
-                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape2.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                                <svg fill="currentColor" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" class="w-85 text-red">
-                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape3.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape1.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                                <svg fill="currentColor" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" class="w-85 text-red">
-                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape2.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                                <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" class="w-85 text-red">
-                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div class="gallery-img-card">
-                        <img class="gallery-img" src="img/landscape3.jpeg" alt="">
-
-                        <div class="gallery-img-text">
-                            <h2>Nandhaka Pieris</h2>
-                        </div>
-                    </div>
-
-                    <div class="gallery-text-card">
-                        <div class="mt-2 text-gray-900 font-light-3 gallery-text">
-                            Laravel has wonderful, thorough documentation covering every aspect of the framework.
-                        </div>
-                        <div class="inline-flex mt-5">
-                            <div class="inline-block box-icon">
-                                <svg fill="currentColor" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" class="w-85 text-red">
-                                    <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                                </svg>
-                            </div>
-                            <div class="inline-block text-gray-700 box-date text-right text-sm font-light-2">
-                                2015-05-02
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+        <Gallery></Gallery>
 
         <Error
             v-if="isErrorVisible"
@@ -199,15 +44,15 @@
             :message="message">
         </Error>
 
-
     </div>
 </template>
 
 <script>
 import Service from '../services/Service';
+import Gallery from './Gallery';
 import Error from './Error';
 export default {
-    components: { Error },
+    components: { Gallery, Error },
     data() {
         return {
             photographer: null,
